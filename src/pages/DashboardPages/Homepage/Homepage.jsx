d6e958c1501232b27b2f4d3e4736c144fd52cd8c
Vincen Oosterom <vincent@oosteromstudio.nlm>
2025-11-16 16:58:02 +0100

    homepage layout gemaakt + responsive

--- src/pages/DashboardPages/Homepage/Homepage.jsx
+++ src/pages/DashboardPages/Homepage/Homepage.jsx
@@ -1,22 +1,27 @@
 import './Homepage.css'
 import SideBar from "../../../components/dashboard/Sidebar/SideBar.jsx";
 import {companies} from "../../../dummy-data/companies.js";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import HeaderDashboard from "../../../components/dashboard/HeaderDashboard/HeaderDashboard.jsx";
+
 
 function Homepage() {
 
     const {companyId} = useParams();
     const company = companies.find((c) => c.companyId === companyId);
+    const navigate = useNavigate();
+
+    function handleGoToAgenda() {
+        navigate(`/dashboard/${companyId}/agenda`);
+    }
 
     return (
         <>
             <div className="dashboard">
                 <SideBar/>
                 <main className="dashboard-main">
-                    <header className="dashboard-header">
-                        <HeaderDashboard title="Welkom terug," company={company.title}/>
-                    </header>
+                    <HeaderDashboard title="Welkom terug," company={company.title}/>
+
 
                     <section className="dashboard-today">
                         <article>
@@ -29,37 +34,58 @@ function Homepage() {
                             <p>{company.nextAppointment}</p>
                         </article>
 
-                        <button className="button-appointment">
-                            <p>Afspraak maken</p>
-                        </button>
+                        <button type="button" className="button-appointment">Afspraak maken</button>
                     </section>
 
-                    <section className="dashboard-quick-agenda">
-                        <h3>Vandaag in één oogopslag</h3>
-                        <table className="today-table">
-                            <tbody>
-                            <tr>
-                                <th>Tijd</th>
-                                <th>Klant</th>
-                                <th>Dienst</th>
-                            </tr>
-                            </tbody>
-                            <tbody>
-                            <tr>
-                                <td>09:00</td>
-                                <td>Karin</td>
-                                <td>Wassen en Knippen</td>
-                            </tr>
-                            </tbody>
-                            <tbody>
-                            <tr>
-                                <td>10:00</td>
-                                <td>Thomas</td>
-                                <td>Knippen & Baard</td>
-                            </tr>
-                            </tbody>
-                        </table>
+                    <section className="quick-look-section">
+
+                        <section className="dashboard-quick-agenda">
+                            <h3>Vandaag in één oogopslag</h3>
+
+                            <table className="today-table">
+                                <thead>
+                                <tr>
+                                    <th>Tijd</th>
+                                    <th>Klant</th>
+                                    <th>Dienst</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>09:00</td>
+                                    <td>Karin</td>
+                                    <td>Wassen en Knippen</td>
+                                </tr>
+                                <tr>
+                                    <td>10:00</td>
+                                    <td>Thomas</td>
+                                    <td>Knippen & Baard</td>
+                                </tr>
+                                </tbody>
+                            </table>
+
+                            <button type="button" onClick={handleGoToAgenda}>
+                                Bekijk volledige agenda
+                            </button>
+                        </section>
+
+                        <article className="dashboard-quick-this-week">
+                            <h3>Weekoverzicht</h3>
+
+                            <ul className="stats-list">
+                                <li>
+                                    <p>Afspraken deze week</p>
+                                    <strong>{company.appointmentsThisWeek}</strong>
+                                </li>
+                                <li>
+                                    <p>Totale omzet</p>
+                                    <strong>€{company.revenuesThisWeek}</strong>
+                                </li>
+                            </ul>
+                        </article>
+
                     </section>
+
                 </main>
             </div>
 
