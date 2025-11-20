aa835f5ac2d88eaf00ceedaba664b1f2eff71ce7
VincentOosterom <vincent@oosteromstudio.nl>
2025-11-20 17:37:25 +0100

    Auth geregeld met token

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,35 @@
+import {useState, useContext, createContext,  useEffect} from "react";
+
+
+const AuthContext = createContext(null)
+
+export function AuthProvider({children}) {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
+    function login(userData) {
+        setUser(userData);
+        localStorage.setItem("user", JSON.stringify(userData));
+    }
+
+    function logout() {
+        setUser(null);
+        localStorage.removeItem("user");
+    }
+
+    return (
+        <AuthContext.Provider value={{user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
